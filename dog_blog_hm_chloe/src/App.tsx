da77ac85a32ba159e3d6d80dbbf7bcc68411654f
rladbgus <cocokiuuu1858@gmail.com>
2021-06-01 13:30:16 +0900

    prettier, eslint 설정

--- dog_blog_hm_chloe/src/App.tsx
+++ dog_blog_hm_chloe/src/App.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        hellomarket
-     </header>
+      <header className="App-header">hellomarket</header>
     </div>
   );
 }
